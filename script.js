7257114885744eb4d0dd28f48b7428edfa9cf49f
צהלה מנשה <saharaz97@gmail.com>
2022-11-02 18:45:18 +0200

    all update

---
+++ script.js
@@ -0,0 +1,17 @@
+const productContainer = document.getElementById("product-container");
+
+const productsList = ["Black Tea", "Green tea"]
+
+
+
+const blackTea = document.createElement("div");
+const greenTea = document.createElement("div");
+
+blackTea.innerHTML = productsList[0];
+greenTea.innerHTML = productsList[1];
+
+blackTea.classList.add("product")
+greenTea.classList.add("product")
+
+productContainer.appendChild(blackTea);
+productContainer.appendChild(greenTea);
